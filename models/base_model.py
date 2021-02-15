c517b6efcae6e4e7b66c70a2e663ac89d363e4b1
tucu <tucu@tucus-MacBook-Pro.local>
2021-02-15 16:31:06 -0300

    fix pep8 task3

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,32 @@
+#!/usr/bin/python3
+""" Module base model """
 from uuid import uuid4
 from datetime import datetime
 
-class BaseModel():
 
+class BaseModel():
+    """ Class Base Model """
     def __init__(self):
-        self.id=uuid4()
-        self.created_at=datetime.now()
-        self.updated_at=datetime.now()
+        """ Init function constructor """
+        self.id = uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.to_dict())
+        """ STR function """
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.to_dict())
 
     def save(self):
-        self.updated_at=datetime.now()
+        """ Save function """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        alm_diccionario=self.__dict__
+        """ ToDict Function """
+        alm_diccionario = self.__dict__
         alm_diccionario["__class__"] = type(self).__name__
-        alm_diccionario["created_at"]= alm_diccionario["created_at"].isoformat()
-        alm_diccionario["updated_at"]= alm_diccionario["updated_at"].isoformat()
+        alm_diccionario["created_at"] = alm_diccionario["created_at"].\
+            isoformat()
+        alm_diccionario["updated_at"] = alm_diccionario["updated_at"].\
+            isoformat()
         return alm_diccionario
