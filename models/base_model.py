5768f8890c3e9828dbdaf6e6db05629ff033b112
tucu <tucu@tucus-MacBook-Pro.local>
2021-02-15 17:46:03 -0300

    add task 4

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,22 @@ from datetime import datetime
 
 class BaseModel():
     """ Class Base Model """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Init function constructor """
-        self.id = uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs.keys():
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.\
+                        strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.\
+                        strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = uuid4()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ STR function """
